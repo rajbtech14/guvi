51be952b389210cfe2a951438f9310cf130252cf
DELL <DELL@DESKTOP-SRM14VE>
2025-02-02 21:56:30 +0530

    commit

--- GuviProject/src/com/tasks/Practice.java
+++ GuviProject/src/com/tasks/Practice.java
@@ -2,9 +2,9 @@ package com.tasks;
 
 public class Practice {
 	public static void main(String[] args) {
-		int i=5;
-		for (int num=0;num<i;i++) {
-			if(num% 2==0 ) {
+		int i=6;
+		for (int num=1;num<i;num++) {
+			if(num% 2!=0 ) {
 				System.out.println(num);	
 			}
 		}
